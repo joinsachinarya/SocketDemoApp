c6d8afc8e56d06b5265afc65a11e911080942ad1
sac6inarya <sachinkarya333@gmail.com>
2023-04-09 20:41:30 +0530

    url added

--- server/index.ts
+++ server/index.ts
@@ -1,7 +1,14 @@
 const express = require("express");
 const app = express();
+// const http = require("http").createServer(app)
 
 const PORT = process.env.PORT || 4001;
+const dir = "/Users/mithyalabs/Desktop/SocketDemo/client/index.html"
+app.get("/", (req: any, res: any) => {
+    res.sendFile(dir);
+    res.send("Hey")
+})
+
 app.listen(PORT, () => {
-    console.log("hey")
+    console.log("On")
 })
