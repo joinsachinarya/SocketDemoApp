23f27c46dbd1bb72492c326732468d58e08d8d45
sac6inarya <sachinkarya333@gmail.com>
2023-04-13 11:05:29 +0530

    done

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
-const express = require("express");
-const App = express();
-const http = require("http").Server(express);
-const Socket = require('socket.io')(http)
+const app = require('express')();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
 const port = process.env.PORT || 3000;
 
-App.get('/', (req, res) => {
+app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
 
-Socket.on('connection', (socket) => {
+io.on('connection', (socket) => {
   socket.on('chatMessage', msg => {
-    Socket.emit('chatMessage', msg);
+    io.emit('chatMessage', msg);
   });
 });
 
+
 http.listen(port, () => {
-  console.log(`Server listening on port: ${port}`);
+  console.log(`Server listening on port ${port}`);
 });
